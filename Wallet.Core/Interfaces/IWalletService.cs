12914caab39d3020b57ed66f595c7c6d3275a205
Ali Taami <alitaami81@gmail.com>
2023-11-17 17:31:14 +0330

    Added WalletService

---
+++ Wallet.Core/Interfaces/IWalletService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet.Core.Interfaces
+{
+    public interface IWalletService
+    {
+        public Task<ServiceResult> ChargeWallet(int amount);
+    }
+}
---
+++ Wallet.Infrastructure/Services/WalletService.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Base;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wallet.Core.Interfaces;
+
+namespace Wallet.Infrastructure.Services
+{
+    public class WalletService : ServiceBase<WalletService>, IWalletService
+    {
+        public WalletService(ILogger<WalletService> logger) : base (logger)
+        {
+
+        }
+        public Task<ServiceResult> ChargeWallet(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
